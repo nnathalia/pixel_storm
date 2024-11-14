b8154d3d01ead29938ec853e7a8a98a17b30848c
nathalia <nmarianolopes@gmail.com>
2024-11-13 21:46:05 -0400

    commit templates :lipstick:

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,6 +5,30 @@ export class AppController {
   @Get('/')
   @Render('home')
   home() {
-    return {};
+    return { title: 'Home' };
+  }
+
+  @Get('/shop')
+  @Render('shop')
+  shop() {
+    return { title: 'Shop' };
+  }
+
+  @Get('/shop_item')
+  @Render('shop_item')
+  shop_item() {
+    return { title: 'Produto' };
+  }
+
+  @Get('/cart')
+  @Render('cart')
+  cart() {
+    return { title: 'Carrinho' };
+  }
+
+  @Get('/about')
+  @Render('about')
+  about() {
+    return { title: 'Sobre Nós' };
   }
 }
