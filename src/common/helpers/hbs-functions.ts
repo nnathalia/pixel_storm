0adf4c9d4bf31e09f53e17feae052802adb17a9f
nathalia <nmarianolopes@gmail.com>
2025-02-22 18:24:24 -0400

    Update

--- src/common/helpers/hbs-functions.ts
+++ src/common/helpers/hbs-functions.ts
@@ -1,3 +1,8 @@
+/* eslint-disable prettier/prettier */
+import moment from "moment";
+import { error } from "console";
+import { json } from "stream/consumers";
+
 const helpers = {
   dateFormat: (date: string, format: string) => {
     const locale = moment(date);
@@ -8,6 +13,14 @@ const helpers = {
     return locale.format(format);
   },
   inc: (value: string) => parseInt(value) + 1,
+
+  json: (value: any) => { return JSON.stringify(value) },
+  'error-message': (error: any[], key: string) => error?.find(i => i.property == key)?.message,
+  'error-messages': (errors: any[], key: string) => errors?.find(i => i.property == key)?.messages,
+  setValue:(valueDefault: any, valueCheck?: any) => {
+    if (!valueCheck) return valueDefault;
+    if(valueDefault && valueCheck) return valueDefault;
+  },
 };
 
 export const hbsRegisterHelpers = (hbs: any) => {
